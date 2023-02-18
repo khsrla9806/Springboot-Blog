73cdec98615324daee7c5d2ba179480190f8a2ba
khsrla9806 <kimhunsope@naver.com>
2023-02-18 20:58:38 +0900

    refactor: User에 RoleType을 적용

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -14,6 +15,7 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+//@DynamicInsert // insert 할 때 null 값은 제외 시켜서 insert 할 수 있다. -> 기능은 좋지만 많이 사용하지 않는 것이 좋다.
 @Entity // User 클래스가 스프링 부트가 실행되면 자동으로 MySQL Table 생성
 public class User {
     @Id // Primary Key
@@ -29,8 +31,9 @@ public class User {
     @Column(nullable = false, length = 50)
     private String email;
 
-    @ColumnDefault("'user'") // ColumnDefault 사용시 ''를 사용해서 넣어줘야 한다. (문자라는 것을 알려주기 위해서)
-    private String role; // 정확하게 하기 위해서는 Enum 사용이 좋다. (Enum 사용시 도메인(범위) 설정이 가능)
+    //@ColumnDefault("'user'") // ColumnDefault 사용시 ''를 사용해서 넣어줘야 한다. (문자라는 것을 알려주기 위해서)
+    @Enumerated(EnumType.STRING) // DB에는 RoleType이라는 것은 없기에 DB에 EnumType은 스트링이라고 알려줘야 한다.
+    private RoleType role; // ADMIN, USER (도메인 지정)
 
     @CreationTimestamp // 시간이 자동으로 입력
     private Timestamp createDate;
