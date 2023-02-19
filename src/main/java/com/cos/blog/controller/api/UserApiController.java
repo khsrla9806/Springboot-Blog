6a4aef454865c7755a93b8f2239dcadcc62b96fe
khsrla9806 <kimhunsope@naver.com>
2023-02-19 22:13:39 +0900

    feat: userService를 사용하여 UserApiController에 회원가입 기능 추가

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.cos.blog.controller.api;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user) {
-        System.out.println("save 호출됨");
-        // TODO: 실제로 DB에 insert 하고 아래에서 리턴이 되도록 작성하면 됨.
-        return new ResponseDto<Integer>(HttpStatus.OK, 1); // 자바 오브젝트를 JSON 으로 변경해서 응답을 보내줌
+        // System.out.println("save 호출됨");
+
+        user.setRole(RoleType.USER);
+        int result = userService.join(user);
+        return new ResponseDto<Integer>(HttpStatus.OK, result); // 자바 오브젝트를 JSON 으로 변경해서 응답을 보내줌
     }
 }
