5ff5f9f5e4dc26942fcae2d417a8db9d73c11851
khsrla9806 <kimhunsope@naver.com>
2023-02-20 13:10:29 +0900

    feat: 로그인 service 로직 구현

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -4,16 +4,20 @@ import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service // 스프링이 컴포넌트 스캔을 통해서 빈으로 등록해줌
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Transactional // 회원가입 전체 서비스가 하나의 트랜잭션이 된다.
+    @Transactional
     public void join(User user) {
         userRepository.save(user); // 여기서 예외가 발생되면 GlobalExceptionHandler 로 가게 되어있다.
+    }
+
+    @Transactional(readOnly = true) // Select 할 때 트랜잭션 시작, 서비스 종료시 종료 (정합성을 유지)
+    public User login(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
     }
 }
