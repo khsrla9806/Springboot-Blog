e7ad28d9663abac308cca9113c7fcd9e9c0c040b
khsrla9806 <kimhunsope@naver.com>
2023-02-20 21:35:40 +0900

    feat: 시큐리티 세션에 넣어줄 UserDetails 타입의 클래스 PrincipalDetail을 추가

---
+++ src/main/java/com/cos/blog/config/auth/PrincipalDetail.java
@@ -0,0 +1,71 @@
+package com.cos.blog.config.auth;
+
+import com.cos.blog.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+// 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails 타입의 오브젝트를
+// 스프링 시큐리티의 고유한 세션저장소에 저장을 한다. 그때 저장되는 것이 UserDetails 타입의 PrincipalDetail 이다.
+public class PrincipalDetail implements UserDetails {
+    private User user; // 객체를 품고있는 것을 컴포지션이라고 한다.
+
+    public PrincipalDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // 계정이 만료되지 않았는지를 리턴한다. (true: 만료안됨)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정이 잠겨있는지 안 잠겨있는지 리턴한다. (true: 잠기지 않음)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 비밀번호가 만료됐는지 안 됐는지를 리턴한다. (true: 만료안됨)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정이 활성화(사용가능)되어있는지 아닌지를 리턴한다. (true: 활성화되어있음)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // 계정이 어떤 권한을 가졌는지를 반환하는 메소드
+    // 권한이 여러개면 for 문을 돌려야 한다.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collectors = new ArrayList<>();
+
+//        collectors.add(new GrantedAuthority() {
+//            @Override
+//            public String getAuthority() {
+//                return "ROLE_" + user.getRole(); // ROLE_USER : 스프링에서 ROLE 을 적는 규칙이다. ROLE_
+//            }
+//        });
+
+        collectors.add(() -> {
+            return "ROLE_" + user.getRole(); // ROLE_USER : 스프링에서 ROLE 을 적는 규칙이다. ROLE_
+        });
+        return collectors;
+    }
+}
