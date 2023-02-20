a078eec86f4442fdbb8a3063378c18fcad0f1e68
khsrla9806 <kimhunsope@naver.com>
2023-02-20 13:10:58 +0900

    feat: 로그인 controller 메서드 구현 (로그인 세션 생성)

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class UserApiController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpSession session;
 
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user) {
@@ -22,5 +27,20 @@ public class UserApiController {
         user.setRole(RoleType.USER);
         userService.join(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바 오브젝트를 JSON 으로 변경해서 응답을 보내줌
+    }
+
+    // 아래는 스프링의 전통적인 로그인 방식
+    // 지금은 사용되지 않는다. Spring Security 사용
+    @PostMapping("/api/user/login")
+    public ResponseDto<Integer> login(@RequestBody User user) {
+        System.out.println("login 호출됨");
+        User principal = userService.login(user); // principal(접근주체)
+
+        // 로그인 세션 만들기
+        if (principal != null) {
+            session.setAttribute("principal", principal);
+        }
+
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 }
