19cd55604d6fe0f69e9dfc96bd1822723c30c097
khsrla9806 <kimhunsope@naver.com>
2023-02-20 21:38:34 +0900

    feat: @AuthenticationPrincipal을 이용해서 컨트롤러에서 세션에 접근할 수 있도록 설정

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,13 +1,18 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.config.auth.PrincipalDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
     @GetMapping({"","/"}) // 아무것도 안 붙였을 때랑 /를 붙였을 때 작동
-    public String index() {
+    // @AuthenticationPrincipal 을 이용해서 세션에 접근할 수 있다.
+    public String index(@AuthenticationPrincipal PrincipalDetail principal) {
         // /WEB-INF/views/index.jsp 찾아감
+        System.out.println("로그인 사용자 아이디: " + principal.getUsername());
         return "index";
     }
 }
