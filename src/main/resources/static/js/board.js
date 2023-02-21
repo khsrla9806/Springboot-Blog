5790cb4598661b36730671e16cbc5b9e211a8075
khsrla9806 <kimhunsope@naver.com>
2023-02-21 19:46:37 +0900

    feat: board.js에서 게시글 수정 PUT 요청을 날릴 수 있도록 ajax 요청 구현

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,11 +1,15 @@
 let index = {
     init: function() {
-        $("#save").on("click", () => {
+        $("#btn-save").on("click", () => {
             this.save();
         });
 
         $("#btn-delete").on("click", () => {
             this.deleteById();
+        });
+
+        $("#btn-update").on("click", () => {
+            this.update();
         });
     },
 
@@ -30,7 +34,8 @@ let index = {
     },
 
     deleteById: function() {
-        var id = $("#id").text();
+        // 태그의 내용을 가져올 때는 text()를 사용
+        let id = $("#id").text();
         $.ajax({
             type: "DELETE",
             url: "/api/board/" + id,
@@ -38,6 +43,30 @@ let index = {
         }).done(function(response) {
             alert("삭제가 완료되었습니다.");
             location.href = "/";
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    update: function() {
+        // 게시글의 id를 가져옴 (태그의 value 값을 가져올 때는 val())
+        let id = $("#id").val();
+
+        // 입력받는 데이터를 가져옴
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        }
+
+        $.ajax({
+            type: "PUT",
+            url: "/api/board/" + id,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function() {
+            alert("수정이 완료되었습니다.");
+            location.href = "/board/" + id;
         }).fail(function(error) {
             alert(JSON.stringify(error));
         });
