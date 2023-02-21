38062509d696d1ae8c4b1613a5da6211fd9e66e4
khsrla9806 <kimhunsope@naver.com>
2023-02-21 17:31:05 +0900

    feat: 모든 게시글 목록을 받아서 index.jsp로 넘기는 작업을 진행 (Model 사용)

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,17 +1,24 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
     @GetMapping({"","/"}) // 아무것도 안 붙였을 때랑 /를 붙였을 때 작동
-    public String index() {
+    public String index(Model model) {
+        // jsp에서 사용할 수 있는 데이터를 들고 갈 때는 Model을 사용한다.
+        // model의 addAttribute로 모든 글 목록을 넣어서 index.jsp로 가져간다.
+        model.addAttribute("boards", boardService.findAllBoard());
         // /WEB-INF/views/index.jsp 찾아감
-        return "index";
+        return "index"; // viewResolver가 작동됨
     }
 
     // User 권한이 필요
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired
@@ -17,5 +19,9 @@ public class BoardService {
         board.setCount(0);
         board.setUser(user); // controller로 부터 가져온 User를 넣어준다. (글쓰기를 요청한 유저)
         boardRepository.save(board);
+    }
+
+    public List<Board> findAllBoard() {
+        return boardRepository.findAll();
     }
 }
