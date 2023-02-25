333747aa3041528ac702f5c9de23de8c1f022dda
khsrla9806 <kimhunsope@naver.com>
2023-02-25 15:16:45 +0900

    feat: 댓글을 최신순(내림차순)으로 가져올 수 있도록 @OrderBy 옵션을 추가

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -39,6 +39,7 @@ public class Board {
     @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy는 나는 연관관계의 주인이 아니다 -> 내 주인은 Reply 클래스에 있는 "board"이다.
     // @JoinColumn: 하지만 제 1정규형을 지키기 위해서 굳이 Board 테이블에 필드를 생성해줄 필요가 없다.
     @JsonIgnoreProperties({"board"}) // 무한 참조를 막기위한 방법 : reply를 호출할 때, reply가 가지고 있는 board는 더이상 참조되지 않는다.
+    @OrderBy("id desc") // 댓글의 id 값을 기준으로 내림차순으로 정렬하여 데이터를 가져다줌
     private List<Reply> reply;
 
     @CreationTimestamp
